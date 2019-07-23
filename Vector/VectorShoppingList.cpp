3fc65ba6cfa6050240c762546733ce606c011050
ATimko <ashleytimko777@gmail.com>
2019-07-23 14:38:19 -0700

    Adding and deleting a list

--- Vector/VectorShoppingList.cpp
+++ Vector/VectorShoppingList.cpp
@@ -16,18 +16,30 @@ int main(){
 	list.push_back("sugar");
 	list.push_back("chocolate");
 	list.push_back("flour");
+	list.push_back("butter");
 	print(list);
 	list.push_back("coffee");
 	print(list);
 
 	//cout << *find(list.begin(), list.end(), "sugar") << endl;
 
+	// Adding
 	for (size_t i = 0; i < list.size(); ++i) {
-		if (list[i] == "sugar") {
+		if (list[i] == "butter") {
 			list[i] = "honey";
 		}
 	}
 	print(list);
+
+	// Removing
+	for (size_t i = 0; i < list.size(); ++i) {
+		if (list[i] == "milk") {
+			list.erase(list.begin() + i);
+			--i;
+		}
+	}
+	print(list);
+
 	system("pause");
 	return 0;
 }
